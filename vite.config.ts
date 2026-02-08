6751249ff3775ed976afb19cbb8fd4617ca436a3
Anton <69308871+antjer94@users.noreply.github.com>
2026-02-08 21:09:50 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-impoIt looksrt react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
