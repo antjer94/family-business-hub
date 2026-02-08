0271a32952e2c151b8dab16c28b265f8593a653b
Anton <69308871+antjer94@users.noreply.github.com>
2026-02-08 22:12:45 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from "vite";
+It import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/family-business-hub/',
   server: {
     host: "::",
     port: 8080,
